492d7276c4c3508552e407f4f907cf41e81f3e67
Jogiter <jogiter.g@gmail.com>
2017-08-14 15:31:23 +0800

    bugfix

--- src/App.vue
+++ src/App.vue
@@ -6,24 +6,21 @@
     <div class="container">
         <div class="title">Formdata 上传 demo</div>
         <!-- Document：https://developer.qiniu.com/kodo/manual/form-upload -->
-        <form id="testform" method="post" enctype="multipart/form-data">
-            <input name="key" id="key" type="hidden" value="">
-            <input name="token" type="hidden" :value="uploadToken">
-            <input id="userfile" name="file" type="file" @change="upload"/>
-            <!-- take photo with phone -->
-            <!-- <input id="userfile" name="file" accept="image/*" type="file" /> -->
 
-            <!-- take video with phone -->
-            <!-- <input id="userfile" name="file" type="file" accept="video/*"/> -->
-            <input name="accept" type="hidden" />
-        </form>
+        <upload :action="action" :token="token" @on-upload="uploadImg" :accept="accept">
+            <p slot="picker" class="upload-btn">
+                <span>上传图片</span>
+            </p>
+        </upload>
 
-        <!-- add file -->
-        <label for="userfile" class="trigger">
-            <span>Trigger =></span>
-            <em>添加文件</em>
-        </label>
-
+        <div class="block">
+            <div id="result">
+                <p>{{hash}}</p>
+                <p>{{key}}</p>
+            </div>
+            <img :src="imgHash" alt="" class="viewimg">
+            <img :src="imgKey" alt="" class="viewimg">
+        </div>
         <!-- upload info -->
         <div class="selected-file"></div>
         <div class="progress"></div>
@@ -36,30 +33,39 @@
 <script>
 import 'vue-address/lib/vue-address.min.css'
 import TAdress from 'vue-address'
-// import upload from './components/upload.vue'
+import upload from './components/upload.vue'
 export default {
     name: 'app',
     components: {
         TAdress,
-        // upload
+        upload
     },
     data: () => {
         return {
-            uploadToken: 'FtFFvQIZOYBiPqSCA7dfdotzy-2uexcYJ3Pjxw9G:eL0jbS0IG-f71ww0ODI5qHqOn6U=:eyJzY29wZSI6InBhZG1vbSIsImRlYWRsaW5lIjoxNTAyNDQ5NjgxfQ=='
+            action: 'http://up-z2.qiniu.com/',
+            // action: 'http://upload.qiniu.com/',
+            accept: 'image/*',
+            token: 'FtFFvQIZOYBiPqSCA7dfdotzy-2uexcYJ3Pjxw9G:UxuBHs_reBxKeMICvw0sQAgiMoM=:eyJzY29wZSI6InBhZG1vbSIsImRlYWRsaW5lIjoxNTAyNjk4MjIzfQ==',
+            hash: '',
+            key: ''
+        }
+    },
+    computed: {
+        imgHash: function () {
+            return `http://img.padmom.com/${this.hash}`
+        },
+        imgKey: function () {
+            return `http://img.padmom.com/${this.key}`
         }
     },
     methods: {
         getAddr(p, c, d) {
             console.log(p, c, d)
         },
-        upload() {
-            let f = new FormData(document.getElementById("testform"))
-            console.log(f)
-            this.$http.post('http://up-z2.qiniu.com/', f).then(function (res) {
-                console.log('suceess')
-                console.log(res)
-            })
-        },
+        uploadImg(res) {
+            this.hash = res.hash
+            this.key = res.key
+        }
     }
 }
 </script>
@@ -98,5 +104,12 @@ form input[type="file"] {
 .trigger {
     border: 1px solid red;
     padding: 15px;
+}
+.block,
+.viewimg {
+    margin: 15px;
+}
+.viewimg {
+    max-width: 200px;
 }
 </style>
--- src/components/upload.vue
+++ src/components/upload.vue
@@ -1,115 +1,71 @@
 <template>
-<div class="upload">
-    <div class="upload-pickfiles-lists" v-for="n in uploadurl.length">
-        <div class="xg-upload-pickfiles">
-            <img :src="uploadurl[n]">
-        </div>
-        <a @click="del($index)">删除</a>
+    <div>
+        <form :id="formId" :method="method" enctype="multipart/form-data">
+            <slot name="form">
+                <input name="token" type="hidden" :value="token">
+                <input name="file" :id="pickerId" type="file" @change="upload" :accept="accept"/>
+                <input name="accept" type="hidden" />
+            </slot>
+        </form>
+
+        <!-- add file -->
+        <label :for="pickerId">
+            <slot name="picker">
+                <em>添加文件</em>
+            </slot>
+        </label>
     </div>
-    <div :id="container" class="upload-container">
-        <div class="upload-pickfiles" :id="pickfiles" v-if="uploadurl.length < max">
-        </div>
-    </div>
-</div>
 </template>
 
-<script type="text/javascript">
-export default {
-    props: {
-        // 最大上传数量
-        max: {
-            type: Number,
-            twoWay: true
-        },
-        // 返回的上传地址
-        uploadurl: {
-            type: Array,
-            twoWay: true,
-        },
-        // 拖拽区域
-        container: {
-            type: String,
-            twoWay: false
-        },
-        // inputID
-        pickfiles: {
-            type: String,
-            twoWay: false
-        },
-        // 多选上传
-        multiple: {
-            type: Boolean,
-            twoWay: false
-        }
-    },
-    ready() {
-        this.qiniu();
-    },
-    methods: {
-
-        qiniu() {
-            const self = this;
-            const uploader = Qiniu.uploader({
-                runtimes: 'html5,flash,html4', //上传模式,依次退化
-                browse_button: self.pickfiles, //上传选择的点选按钮，**必需**
-                uptoken_url: '/token', //Ajax请求upToken的Url，**强烈建议设置**（服务端提供）
-                // uptoken : '', //若未指定uptoken_url,则必须指定 uptoken ,uptoken由其他程序生成
-                // unique_names: true, // 默认 false，key为文件名。若开启该选项，SDK为自动生成上传成功后的key（文件名）。
-                // save_key: true,   // 默认 false。若在服务端生成uptoken的上传策略中指定了 `sava_key`，则开启，SDK会忽略对key的处理
-                domain: 'http://qiniu-plupload.qiniudn.com/', //bucket 域名，下载资源时用到，**必需**
-                get_new_uptoken: false, //设置上传文件的时候是否每次都重新获取新的token
-                container: self.container, //上传区域DOM ID，默认是browser_button的父元素，
-                max_file_size: '100mb', //最大文件体积限制
-                flash_swf_url: 'js/plupload/Moxie.swf', //引入flash,相对路径
-                max_retries: 3, //上传失败最大重试次数
-                dragdrop: true, //开启可拖曳上传
-                drop_element: 'container', //拖曳上传区域元素的ID，拖曳文件或文件夹后可触发上传
-                chunk_size: '4mb', //分块上传时，每片的体积
-                auto_start: true, //选择文件后自动上传，若关闭需要自己绑定事件触发上传
-                multi_selection: self.multiple, //多选上传
-                filters: {
-                    max_file_size: '100mb',
-                    prevent_duplicates: true,
-                    mime_types: [{
-                            title: 'Image files',
-                            extensions: 'JPEG,jpg,png'
-                        }, // 限定jpg,gif,png后缀上传
-                    ]
-                },
-                init: {
-                    // 'FilesAdded': function (up, files) {
-                    //     $('table').show();
-                    //     $('#success').hide();
-                    //     plupload.each(files, function (file) {
-                    //     });
-                    // },
-                    // BeforeUpload(up, file) {
-                    // },
-                    // 'UploadProgress': function (up, file) {
-
-                    // },
-                    // 'UploadComplete': function () {
-
-                    // },
-                    FileUploaded(up, file, info) {
-                        const domain = up.getOption('domain');
-                        const res = JSON.parse(info);
-                        const sourceLink = domain + res.key;
-                        self.uploadurl.push(sourceLink);
-                        console.info(self.uploadurl);
-                    },
-                    Error(up, err, errTip) {
-                        // $('table').show();
-                        // const progress = new FileProgress(err.file, 'fsUploadProgress');
-                        // progress.setError();
-                        // progress.setStatus(errTip);
-                    }
+<script>
+    export default {
+        props: {
+            method: {
+                type: String,
+                default: 'post'
+            },
+            action: {
+                type: String,
+                default: ''
+            },
+            token: {
+                type: String,
+                default: ''
+            },
+            accept: {
+                type: String,
+                default: 'image/*'
+            },
+            callback: {
+                type: Function,
+                default: () => {
+                    console.log('upload done');
                 }
-            });
+            }
         },
-        del(index) {
-            this.uploadurl.splice(index, 1);
+        data: function () {
+            return {
+                key: '',
+                formId: `upload${new Date().getTime()}`,
+                pickerId: `picker${new Date().getTime()}`
+            }
+        },
+        methods: {
+            upload() {
+                this.key = Math.random().toString(36).substr(2) + document.getElementById(this.pickerId).value.match(/\.?[^.\/]+$/)
+                this.$nextTick(() => {
+                    let formData = new FormData(document.getElementById(this.formId))
+                    this.$http.post(this.action, formData).then(res => {
+                        this.$emit('on-upload', res.body)
+                    })
+                })
+            }
         }
     }
-};
 </script>
+
+<style scoped>
+    form {
+        display: none;
+    }
+</style>
